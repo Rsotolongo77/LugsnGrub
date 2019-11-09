45bc8ca2ace06765c803498e49297946e81c536d
Ricardo Sotolongo <rickey.soto@gmail.com>
2019-11-09 00:16:24 -0500

    continuing to try different adjustments to get app to deploy on heroku.

--- server.js
+++ server.js
@@ -34,8 +34,11 @@ app.use(flash());
 
 /* Serve up static assets (usually on heroku) */
 if (process.env.NODE_ENV === "development") {
-  app.use(passport.session()); app.use(express.static(path.join(__dirname, '../build')));
+  app.use(passport.session()); app.use(express.static(__dirname + '/client/build'))
 };
+
+app.use(express.static(__dirname + '/client/build'))
+// app.use(express.static(path.join(__dirname, '../build')));
 
 app.use(fileUpload())
 
